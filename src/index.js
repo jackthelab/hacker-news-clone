209a13af314660a0f696e5119a6c5841720e203b
Jacob Hampton <jhampton792@gmail.com>
2021-06-18 15:50:21 -0500

    Updated with test data and fixed resolvers for Query/added resolver for Link

--- src/index.js
+++ src/index.js
@@ -3,12 +3,39 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
+// dummy data
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  },
+  {
+    id: 'link-1',
+    url: 'www.howtographql.com/graphql-js',
+    description: 'A deeper dive specifically for GraphQL in JavaScript'
+  }
+]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackerclone News`
+    info: () => `This is the API of a Hackerclone News`,
+    feed: () => links
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
   }
 }
 
