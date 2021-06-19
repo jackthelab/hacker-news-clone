cd796dcfdc082a8633a7d674e0a39f2a37602585
Jacob Hampton <jhampton792@gmail.com>
2021-06-19 14:42:13 -0500

    added CRUD actions within same server session -- not currently persisting data though.

--- src/index.js
+++ src/index.js
@@ -16,10 +16,15 @@ let links = [
   }
 ]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackerclone News`,
-    feed: () => links
+    feed: () => links,
+    link: (parent, { id }) => {
+      return links.find( (link) => link.id === id )
+    }
   },
   Mutation: {
     post: (parent, args) => {
@@ -30,6 +35,39 @@ const resolvers = {
       }
       links.push(link)
       return link
+    },
+    updateLink: (parent, args) => {
+      // find the desired link based on id passed in args
+      const selectedLink = links.find( (link) => link.id == args.id );
+
+      // make desired changes to the link
+      if(args.url) {
+        selectedLink.url = args.url;
+      }
+      if(args.description) {
+        selectedLink.description = args.description;
+      }
+
+      //return the updated link
+      return selectedLink;
+    },
+    deleteLink: (parent, { id }) => {
+      // find index of desired link based on id passed in args, abstracted to { id } here
+      const selectedLinkIndex = links.findIndex( (link) => link.id === id );
+
+      const startLength = links.length;
+
+      // delete link at above index from links list.
+      let removedLink = links[selectedLinkIndex]
+      links.splice(selectedLinkIndex, 1);
+
+      // return deleted Link
+      if(links.length + 1 === startLength) {
+        // return `Successfully deleted ${ id }`;
+        return removedLink;
+      } else {
+        return 'Failed to remove link'
+      }
     }
   },
   // The entire Link resolver can be commented out since the only implementations we're using are trivial here
@@ -42,7 +80,7 @@ const resolvers = {
   // }
 }
 
-// GraphQLServer allows us to reference for typeDefs, as below, instead of forcing us to have string variable in the same file
+// GraphQLServer allows us to reference external files with 'fs' for typeDefs, as below, instead of forcing us to have string variable in the same file
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(
     path.join(__dirname, 'schema.graphql'),
